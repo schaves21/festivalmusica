d40f3f3aa4db6f3d9b580698c605c51a8ac30f9f
schaves21 <schaves.ap@gmail.com>
2024-07-28 19:19:57 -0300

    Aplicando performance web minificando app.css y app.js, lazy loading en imagenes, imagenes mas livianas con libreria sharp, imagenes webP y AVIF

--- src/js/app.js
+++ src/js/app.js
@@ -23,9 +23,12 @@ function crearGaleria() {
   const galeria = document.querySelector(".galeria-imagenes");
 
   for (let i = 1; i <= CANTIDAD_IMAGENES; i++) {
-    const imagen = document.createElement("IMG");
-    imagen.src = `src/img/gallery/full/${i}.jpg`;
-    imagen.alt = "Imagen Galería";
+    const imagen = document.createElement("PICTURE");
+    imagen.innerHTML = `
+          <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+          <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+          <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+      `;
 
     // Event Handler
     imagen.onclick = function () {
@@ -37,9 +40,12 @@ function crearGaleria() {
 }
 
 function mostrarImagen(i) {
-  const imagen = document.createElement("IMG");
-  imagen.src = `src/img/gallery/full/${i}.jpg`;
-  imagen.alt = "Imagen Galería";
+  const imagen = document.createElement("PICTURE");
+  imagen.innerHTML = `
+      <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+      <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+      <img loading="lazy" width="200" height="300" src="build/img/gallery/full/${i}.jpg" alt="imagen galeria">
+  `;
 
   // Generar Modal
   const modal = document.createElement("DIV");
